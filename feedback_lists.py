8ba278b4d6e7450116a4cd95b90fb9b617d0a23c
Smokesick <smokesick_nn@yahoo.com>
2015-10-13 21:01:25 +0100

    Exercise #2 Merged with our code and altered a bit
    A lot was changed in terms of functions and functionality for the basic mechanics to work. Type "help" to see which commands we currently have and are usable. For the moment we can move only to Hangar #1 and back to Player Ship, as we will have to fix the naming of each room.

--- feedback_lists.py
+++ feedback_lists.py
@@ -4,7 +4,8 @@ from collections import OrderedDict
 commands_expanded = {"playername": ["playername, name, changename"]}
 
 commands = OrderedDict([("go", "moves the player"),
-                        ("inspect", "inspect the details of an element"), ("exits", "shows all available exits"),
+                        ("inspect", "inspect the details of an element"), ("inventory", "see what you are carrying"),
+                        ("exits", "shows all available exits"),
                         ("help", "shows a list of available commands"), ("save", ""), ("load", ""), ("quit", "quits the game"),
                         ("playername", "change player's name")])
 
@@ -16,3 +17,8 @@ command_unknown = ["Am I drunk? Let me try that again.",
                    "What will mom say if I did that?",
                    "I do not want to be chased by the FBI."
                    ]
+
+go_deny = ["Going there is not an option.",
+           "I cannot go there.",
+           "What's in it for me by going there?",
+           "This location is unfamiliar to me."]
