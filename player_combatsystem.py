f86d0c39120509c8b4a292652ffa9d70e9f07604
Elliott Reynolds <elliott.rey@gmail.com>
2015-10-19 12:59:59 +0100

    Combat system files to implement

---
+++ player_combatsystem.py
@@ -0,0 +1,29 @@
+from items_combatsystem import *
+
+#def print_player(name_input, stats): 
+
+	#name = name_input
+
+	#player={"Name": name, "HP": 100, "Current Weapon": sword}
+
+	#for key in player:
+		#print(key + " is " + str(player[key]))
+
+	#print (name)
+
+enemy={"Name":"Kirill", "HP": 100, "Current Weapon": "spoon"}
+elliott={"Name": "Elliott", "HP": 100, "Current Weapon": "Sword"}
+
+	
+
+	
+
+
+
+
+
+
+
+
+
+
