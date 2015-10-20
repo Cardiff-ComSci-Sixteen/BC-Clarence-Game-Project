19603cb7a3fcc61db8826fa9d8f80b3aa0686813
Elliott Reynolds <elliott.rey@gmail.com>
2015-10-20 12:07:49 +0100

    Updated enemies into there 'monster.py' file from 'player.py

--- player.py
+++ player.py
@@ -22,12 +22,4 @@ current_room = rooms["Wrecked Ship"]
 in_room = "Wrecked Ship"
 
 #Boss enemy, maybe change the variable name to enemy_boss?
-#Accuracy is high and medium-high chance to dodge.
-enemy = {"name": "Kirill", "hp": 100, "weapon": ["Spoon" , "Rocket Launcher" , "Laptop"] , "damage": [randint(15,20), randint(15,30), 5]}
-#3 Various enemies which can be around the map. Need too assign accuracy and dodge still. 
-#Accuracy is low for the gun but high for the sword for enemy_1 and low dodge chance
-enemy_1 = {"name": "Volderwart", "hp": 20, "weapon": ["Gun", "Sword"] , "damage": [randint(5,10), randint(1,10)]}
-#Accuracy is low for enemy_2 and medium dodge chance
-enemy_2 = {"name": "Potter", "hp": 40, "weapon": "Mini Pistol", "damage": randint(10,30)}
-#Accuracy is medium for enemy_3 and medium dodge chance
-enemy_3 = {"name": "Matt", "hp": 50, "weapon": "Battle Axe", "damage": randint(10,20)}
+enemy = {"name": "Kirill", "hp": 100, "weapon": "spoon"}
